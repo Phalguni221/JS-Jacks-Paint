b02281f7100db16521f98fb882ee49fbe632c8b5
Sriya Bhamidipati <88012398+Phalguni221@users.noreply.github.com>
2021-11-12 21:18:48 -0500

    JS-Jacks-Paint

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,98 +1,59 @@
-function configureListeners() {
-    var images = document.getElementsByTagName('img');    
 
-     for (var i = 0; i < images.length; i++) {        
-        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
-        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
-    } 
-}
 
-function addOpacity(event) {
-    if (!this.classList.contains('dim')){
-        this.classList.add('dim')
-    }    
-    getProductInfo(event.target.id);     
-}
 
-function removeOpacity(event) {
-     if (this.classList.contains('dim')){
-        this.classList.remove('dim');
-    }
+// Color	Price per Gallon
+// Lime Green	$14.99
+// Medium Brown	$11.14
+// Royal Blue	$22.99
+// Solid Black	$4.99
+// Solid Cyan	$8.22
+// Solid Purple	$11.99
+// Solid Red	$13.42
+// Solid White	$21.98
+// Solid Yellow	$14.99
 
-    let element = document.getElementById('ppg');
-        element.textContent = '';
-        
-    let color = document.getElementById('color');
-        color.textContent = ''; 
 
-    event.preventDefault();    
-}
+let pn1 = document.querySelector('#pn1');
+pn1.addEventListener('mouseover', function(event) {
+    alert('Lime green. Price: $14.99');
+});
 
-function changeImage(elementId) {
-    let image = document.getElementById('imgDisplay');
-    image.src = elementId.src;
-}
+let pn2 = document.querySelector('#pn2')
+pn2.addEventListener('mouseover', function(event) {
+    alert('Medium Brown. Price:$11.14');
+});
 
-function getProductInfo(partNumber) {
-    let price;
-    let colorName;  
-    
-    switch (partNumber) {
-        case 'pn1':           
-            price = '$19.99'
-            colorName = 'Lime Green'
-            updatePrice(colorName, price)           
-            break;           
-        case 'pn2':
-            price = '$12.99'
-            colorName = 'Medium Brown' 
-            updatePrice(colorName, price)     
-            break;            
-        case 'pn3':
-            price = '$11.99'
-            colorName = 'Royal Blue'   
-            updatePrice(colorName, price)   
-            break;   
-        case 'pn4':
-            price = '$14.99'
-            colorName = 'Bright Red'            
-            updatePrice(colorName, price)   
-            break;   
-        case 'pn5':
-            price = '$9.99'
-            colorName = 'Solid White' 
-            updatePrice(colorName, price)              
-            break;   
-        case 'pn6':
-            price = '$15.99'
-            colorName = 'Solid Black'   
-            updatePrice(colorName, price)            
-            break;   
-        case 'pn7':
-            price = '$8.99'
-            colorName = 'Medium Blue'       
-            updatePrice(colorName, price)        
-            break;   
-        case 'pn8':
-            price = '$16.99'
-            colorName = 'Light Purple'      
-            updatePrice(colorName, price)         
-            break;   
-        case 'pn9':
-            price = '$17.99'
-            colorName = 'Bright Yellow'                       
-            updatePrice(colorName, price)   
-            break;   
-          default:              
-    }
+let pn3 = document.querySelector('#pn3')
+pn3.addEventListener('mouseover', function(event) {
+    alert('Royal Blue. Price:$22.99');
+});
 
-    function updatePrice(colorName, price)
-    {       
-        let ppg = document.getElementById('ppg');
-        ppg.textContent = price;
-        
-        let color = document.getElementById('color');
-        color.textContent = colorName;
-    }
-    
-}
+let pn4 = document.querySelector('#pn4')
+pn4.addEventListener('mouseover', function(event) {
+    alert('Solid Red. Price:$13.42');
+});
+
+let pn5 = document.querySelector('#pn5')
+pn5.addEventListener('mouseover', function(event) {
+    alert('Solid White. Price:$21.98');
+});
+
+let pn6 = document.querySelector('#pn6')
+pn6.addEventListener('mouseover', function(event) {
+    alert('Solid Black - $4.99');
+});
+
+let pn7 = document.querySelector('#pn7')
+pn7.addEventListener('mouseover', function(event) {
+    alert('Solid Cyan. Price:$8.22');
+});
+
+let pn8 = document.querySelector('#pn8')
+pn8.addEventListener('mouseover', function(event) {
+    alert('Solid Purple. Price:$11.99');
+});
+
+let pn9 = document.querySelector('#pn9')
+pn9.addEventListener('mouseover', function(event) {
+    alert('Solid Yellow. Price:$14.99');
+});
